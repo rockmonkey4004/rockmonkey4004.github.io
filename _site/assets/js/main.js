4486e4bb03fe811cc445c9207c6e6ba4aa003828
ls <55054729+rockmonkey4004@users.noreply.github.com>
2020-04-24 13:14:37 -0500

    .

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -121,7 +121,7 @@
       data: $(this).serialize(),
       contentType: 'application/x-www-form-urlencoded',
       success: function (data) {
-        showModal('Comment submitted', 'Thanks! Your comment is <a href="https://github.com/travisdowns/travisdowns.github.io/pulls">pending</a>. It will appear when approved.');
+        showModal('Comment submitted', 'Thanks! Your comment is <a href="https://github.com/rockmonkey4004/rockmonkey4004.github.io/pulls">pending</a>. It will appear when approved.');
 
         $("#comment-form-submit")
           .html("Submit");
